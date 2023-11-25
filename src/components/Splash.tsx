1b506c26bda9825095eedbb4778ba0edf0d9368a
da-in <da-in@wavve.com>
2023-11-26 06:07:36 +0900

    Splash

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,7 +1,7 @@
 const Splash = () => {
     return (
         <div className="text-white font-bold w-full h-full fixed top-0 left-0 bg-black flex justify-center items-center z-40">
-            <h1 className="splash">Service Name</h1>
+            <span className="splash text-3xl text-[#E21401]" >AI Blur</span>
         </div>
     )
 }

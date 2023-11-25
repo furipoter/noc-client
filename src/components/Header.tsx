233b4d883900279b9192a002a125d70ef1b5a2ef
da-in <da-in@wavve.com>
2023-11-26 04:41:28 +0900

    Upload Video File

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 import { MdOutlineExitToApp } from "react-icons/md";
 import {useNavigate} from "react-router-dom";
 
-const Header = () => {
+const Header = ({title}:{title:string}) => {
     const navigate = useNavigate()
     const onClick = () => {
         navigate(-1)
     }
     return (
-        <div className="flex w-full justify-between p-6 gradient">
-            <div className="text-red font-bold text-2xl break-keep">[LIVE] FURIOSA AI 해커톤 현장</div>
+        <div className="fixed top-0 left-0 flex w-full justify-between p-6 gradient z-40">
+            <div className="text-red font-bold text-2xl break-keep">{title}</div>
             <button onClick={onClick}>
                 <MdOutlineExitToApp size="24" />
             </button>
--- src/pages/LivePage.tsx
+++ src/pages/LivePage.tsx
@@ -1,7 +1,6 @@
-import {useRef, useEffect, useState} from "react";
+import {useRef, useEffect} from "react";
 import Header from "../components/Header.tsx";
-import { FaRecordVinyl, FaCircleStop } from "react-icons/fa6";
-import people from '../assets/people.mp4'
+import { IoMdCloudUpload } from "react-icons/io";
 
 const CONSTRAINTS = {
     video: true,
@@ -9,42 +8,42 @@ const CONSTRAINTS = {
 };
 
 const LivePage = () => {
-    const isLiveOnRef = useRef(false);
-    const [isLiveOn, setIsLiveOn] = useState(false);
+    // const isLiveOnRef = useRef(false);
+    // const [isLiveOn, setIsLiveOn] = useState(false);
     const liveRef = useRef<HTMLVideoElement>(null);
     const mediaRecorderRef = useRef<MediaRecorder | null>(null);
     // const chunks: Blob[] = [];
 
-    let sequence = 0
+    // let sequence = 0
 
-    const uploadVideo = async (blob: Blob) => {
-        if (!isLiveOnRef.current) {
-            return;
-        }
-        console.log('uploadVideo', blob);
-        // const mp4 = new Blob([people], { type: 'video/mp4' });
-        try {
-            const formData = new FormData();
-            formData.append('file_name', String(sequence)+'.mp4');  // 원하는 파일 이름으로 설정
-            formData.append('video', people);
-
-            const response = await fetch('http://13.209.86.34:5001/api/video/upload', {
-                method: 'POST',
-                body: formData,
-            });
-
-            sequence += 1
-
-            if (response.ok) {
-                const responseData = await response.json();
-                console.log('서버 응답:', responseData);
-            } else {
-                console.error('서버 응답 오류:', response.status, response.statusText);
-            }
-        } catch (error) {
-            console.error('API 요청 중 에러:', error);
-        }
-    };
+    // const uploadVideo = async (blob: Blob) => {
+    //     if (!isLiveOnRef.current) {
+    //         return;
+    //     }
+    //     console.log('uploadVideo', blob);
+    //     // const mp4 = new Blob([people], { type: 'video/mp4' });
+    //     try {
+    //         const formData = new FormData();
+    //         formData.append('file_name', String(sequence)+'.mp4');  // 원하는 파일 이름으로 설정
+    //         formData.append('video', blob);
+    //
+    //         const response = await fetch('http://14.35.173.13:38228/api/video/upload', {
+    //             method: 'POST',
+    //             body: formData,
+    //         });
+    //
+    //         sequence += 1
+    //
+    //         if (response.ok) {
+    //             const responseData = await response.json();
+    //             console.log('서버 응답:', responseData);
+    //         } else {
+    //             console.error('서버 응답 오류:', response.status, response.statusText);
+    //         }
+    //     } catch (error) {
+    //         console.error('API 요청 중 에러:', error);
+    //     }
+    // };
 
     const startLive = async () => {
         try {
@@ -61,7 +60,7 @@ const LivePage = () => {
                 // 녹화가 준비되었을 때 이벤트 처리
                 mediaRecorderRef.current.ondataavailable = (e) => {
                     if (e.data.size > 0){
-                        uploadVideo(e.data);
+                        // uploadVideo(e.data);
                         // console.log('onDataAvailable', e.data);
                     }
                 };
@@ -80,9 +79,9 @@ const LivePage = () => {
         }
     };
 
-    useEffect(() => {
-        isLiveOnRef.current = isLiveOn;
-    }, [isLiveOn]);
+    // useEffect(() => {
+    //     isLiveOnRef.current = isLiveOn;
+    // }, [isLiveOn]);
 
     // 컴포넌트가 언마운트될 때 녹화 중지
     useEffect(() => {
@@ -94,46 +93,50 @@ const LivePage = () => {
         };
     }, []);
 
-    const onClick = () => {
-        // isLiveOn 값을 토글
-        setIsLiveOn(!isLiveOn);
-    };
+    // const onClick = () => {
+    //     // isLiveOn 값을 토글
+    //     setIsLiveOn(!isLiveOn);
+    // };
 
-    const fileInputRef = useRef(null);
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-    // const handleFileChange = () => {
-    //     const file = fileInputRef?.current?.files[0];
-    //
-    //     if (file) {
-    //         const formData = new FormData();
-    //         formData.append("video", file);
-    //         formData.append('file_name', 'people_test.mp4');
-    //
-    //         // 여기서 formData를 서버로 전송하거나, axios 등을 사용하여 업로드합니다.
-    //         // 아래는 fetch를 사용한 예제입니다.
-    //         fetch("http://13.209.86.34:5001/api/video/upload", {
-    //             method: "POST",
-    //             body: formData,
-    //         })
-    //             .then((response) => response.json())
-    //             .then((data) => {
-    //                 console.log("Upload success:", data);
-    //             })
-    //             .catch((error) => {
-    //                 console.error("Upload error:", error);
-    //             });
-    //     }
-    // }
+    const handleFileChange = () => {
+        const file = fileInputRef?.current?.files?.[0];
+
+        if (file) {
+            const formData = new FormData();
+            formData.append("video", file);
+            formData.append('file_name', 'people_test.mp4');
+
+            // 여기서 formData를 서버로 전송하거나, axios 등을 사용하여 업로드합니다.
+            // 아래는 fetch를 사용한 예제입니다.
+            fetch("http://14.35.173.13:38228/api/video/upload", {
+                method: "POST",
+                body: formData,
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log("Upload success:", data);
+                })
+                .catch((error) => {
+                    console.error("Upload error:", error);
+                });
+        }
+    }
 
     return (
         <>
             <video className="video" width="100%" autoPlay ref={liveRef} />
-            <div className="fixed top-0 left-0 w-full">
-                <Header />
-                <div className="fixed p-6 right-0">
-                    <button className="p-[2px] bg-white rounded-3xl" onClick={onClick}> {isLiveOn ?  <FaCircleStop color="#E21401" size="48"/> : <FaRecordVinyl color="#E21401" size="48"/>} </button>
+            <div className="fixed top-0 left-0 w-full h-full">
+                <Header title="AI BLUR 콘텐츠 등록"/>
+                {/*<div className="fixed p-6 right-0">*/}
+                {/*    <button className="p-[2px] bg-white rounded-3xl" onClick={onClick}> {isLiveOn ?  <FaCircleStop color="#E21401" size="48"/> : <FaRecordVinyl color="#E21401" size="48"/>} </button>*/}
+                {/*</div>*/}
+                <div className="h-full flex flex-col justify-center items-center backdrop-blur">
+                    <label htmlFor="file" className="font-bold">블러처리 할 파일을 등록해주세요.</label>
+                    <label htmlFor="file" ><IoMdCloudUpload color="#FFFFFF" size="72" /></label>
+                    <input id="file" type="file" className="hidden" ref={fileInputRef} onChange={handleFileChange}/>
                 </div>
-                <input type="file" ref={fileInputRef} onChange={handleFileChange} />
             </div>
         </>
     );
